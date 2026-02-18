d9fd58fb3e60919877e01cf9dcdf9b1bf11cc10f
Natasha Benjamin <bnatasha@wustl.edu>
2026-02-18 14:01:15 -0600

    studio 4 done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,27 @@
 package studio4;
 
-import java.awt.Color;
 
+import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.clear();
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.setPenRadius(0.02);
+		StdDraw.rectangle(0.5, 0.5, 0.4, 0.3);
+		Color customColor = new Color(255, 105, 180);
+		StdDraw.setPenColor(customColor);
+		StdDraw.filledRectangle(0.5, 0.35, 0.4, 0.15);
 		
+		StdDraw.setPenColor(255, 209, 220);
+		
+		StdDraw.filledRectangle(0.5, 0.65, 0.4, 0.15);
+	
+	
+	
+	
+	
 	}
-}
+
+	}
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -21,7 +21,7 @@ public class InterpretDrawingFile {
 		Scanner in = new Scanner(f); //making Scanner with a File
 		StdDraw.clear();
 		StdDraw.setPenColor(Color.BLUE);
-		StdDraw.setPenRadius(0.05);
+		StdDraw.setPenRadius(0.02);
 		
 	String shape = in.next();	
 	if (shape.equals("rectangle")) {
@@ -32,16 +32,67 @@ public class InterpretDrawingFile {
 		Color customColor = new Color(redValue, greenValue, blueValue);
 		StdDraw.setPenColor(customColor);
 		boolean Filled = in.nextBoolean();
-		int x = in.nextInt();
-		int y = in.nextInt();
-		int halfWidth = in.nextInt();
-		int halfHeight = in.nextInt();
-		StdDraw.rectangle(x, y, halfWidth, halfHeight);
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+		
+		if (Filled) {
+			StdDraw.filledRectangle(x, y, halfWidth, halfHeight);
+		} else {
+			StdDraw.rectangle(x, y, halfWidth, halfHeight);
+			}
+		}
+	else if (shape.equals("ellipse")) {
+		int redValue = in.nextInt();
+		int greenValue = in.nextInt();
+		int blueValue = in.nextInt();
+		Color customColor = new Color(redValue, greenValue, blueValue);
+		StdDraw.setPenColor(customColor);
+		boolean Filled = in.nextBoolean();
+		double x = in.nextDouble();
+		double y = in.nextDouble();
+		double halfWidth = in.nextDouble();
+		double halfHeight = in.nextDouble();
+	if (Filled) {
+			StdDraw.filledEllipse(x, y, halfWidth, halfHeight);
+		} else {
+			StdDraw.ellipse(x, y, halfWidth, halfHeight);
+			}
+		}
+else if (shape.equals("triangle")) {
+		int redValue = in.nextInt();
+		int greenValue = in.nextInt();
+		int blueValue = in.nextInt();
+		Color customColor = new Color(redValue, greenValue, blueValue);
+		StdDraw.setPenColor(customColor);
+		boolean Filled = in.nextBoolean();
+		double x1 = in.nextDouble();
+		double x2 = in.nextDouble();
+		double x3 = in.nextDouble();
+		double y1 = in.nextDouble();
+		double y2 = in.nextDouble();
+		double y3 = in.nextDouble();
+
+		double[] coord1 = { x1, x2, x3 };
+        double[] coord2 = { y1, y2, y3 };
+
+
+	if (Filled) {
+			StdDraw.filledPolygon(coord1, coord2);
+		}
+		else {
+			StdDraw.polygon(coord1, coord2);
+			}
+		}
+
+	}
 
 
 	} 
+
 	
 
 		
-	}
-}
+	
+
