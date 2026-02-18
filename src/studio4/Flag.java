9f9547f10c1087389cb370320ef9cd13068e5283
Natasha Benjamin <bnatasha@wustl.edu>
2026-02-18 14:10:43 -0600

    update

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -3,6 +3,9 @@ package studio4;
 
 import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
+import java.awt.Font;
+
+
 
 public class Flag {
 	public static void main(String[] args) {
@@ -17,8 +20,10 @@ public class Flag {
 		StdDraw.setPenColor(255, 209, 220);
 		
 		StdDraw.filledRectangle(0.5, 0.65, 0.4, 0.15);
-	
-	
+	StdDraw.setPenColor(Color.BLACK);
+		StdDraw.setScale(0, 100);   // x and y both go from 0 to 100
+StdDraw.setFont(new Font("Monospaced", Font.BOLD, 30));
+        StdDraw.text(50, 52, "women in STEM<3");
 	
 	
 	
