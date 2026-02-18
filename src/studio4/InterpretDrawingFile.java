e51e0d34753d01e292a21d8370385322290a2b84
Natasha Benjamin <bnatasha@wustl.edu>
2026-02-18 13:27:38 -0600

    update

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -3,7 +3,7 @@ package studio4;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
-
+import java.awt.Color;
 import javax.swing.JFileChooser;
 import javax.swing.SwingUtilities;
 
@@ -19,6 +19,29 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
+		StdDraw.clear();
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.setPenRadius(0.05);
+		
+	String shape = in.next();	
+	if (shape.equals("rectangle")) {
+		
+		int redValue = in.nextInt();
+		int greenValue = in.nextInt();
+		int blueValue = in.nextInt();
+		Color customColor = new Color(redValue, greenValue, blueValue);
+		StdDraw.setPenColor(customColor);
+		boolean Filled = in.nextBoolean();
+		int x = in.nextInt();
+		int y = in.nextInt();
+		int halfWidth = in.nextInt();
+		int halfHeight = in.nextInt();
+		StdDraw.rectangle(x, y, halfWidth, halfHeight);
+
+
+	} 
+	
+
 		
 	}
 }
